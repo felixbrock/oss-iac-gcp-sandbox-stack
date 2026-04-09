b3d9f0b0f17cc6fc9cfbd15430e3096051967e58
Felix Brockmeier <fe.brockmeier@gmail.com>
2026-04-09 20:33:37 +0100

    simulate destroy

--- main.tf
+++ main.tf
@@ -7,16 +7,5 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-resource "google_storage_bucket" "sandbox" {
-  name                        = "${var.bucket_prefix}-${random_id.bucket_suffix.hex}"
-  location                    = var.region
-  force_destroy               = false
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced"
 
-  labels = {
-    managed_by = "oss-iac"
-    purpose    = "sandbox-test"
-  }
-}
 
